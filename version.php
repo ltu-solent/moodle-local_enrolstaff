16427fea59c59f9d9fe9852d7beedf0e40bf993b
Mark Sharp <mark.sharp@solent.ac.uk>
2022-07-13 10:15:21 +0100

    Auto add to usermenu

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php
-$plugin->version  = 2021120701;   // The (date) version of this plugin
+$plugin->version  = 2021120702;   // The (date) version of this plugin
+$plugin->release = 'v2021120702';
 $plugin->requires = 2020061500;   // Requires this Moodle version
 $plugin->component = 'local_enrolstaff';
