0322ede7d5134aecae1f0b5067793561ec40e18b
Mark Sharp <mark.sharp@solent.ac.uk>
2023-03-15 09:30:06 +0000

    Enclose settings in hasconfig check

--- settings.php
+++ settings.php
@@ -25,41 +25,42 @@
 defined('MOODLE_INTERNAL') || die;
 
 $settings = new admin_settingpage('local_enrolstaff', new lang_string('pluginname', 'local_enrolstaff'));
+if ($hassiteconfig) {
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/unitleaderid', get_string('unitleaderid', 'local_enrolstaff'), '', '19'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/roleids', get_string('roleidssolent', 'local_enrolstaff'), '', '0'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/qaheroleids', get_string('roleidsqahe', 'local_enrolstaff'), '', '0'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/qahecodes', get_string('codesqahe', 'local_enrolstaff'), '', '0'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/excludeshortname', get_string('excludeshortname', 'local_enrolstaff'), '', '0'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/excludefullname', get_string('excludeshortname', 'local_enrolstaff'), '', 'counselling,social work'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/excludeid', get_string('excludeid', 'local_enrolstaff'), '', '328, 22679, 6432'));
+    $settings->add(new admin_setting_configtext(
+        'local_enrolstaff/studentrecords', get_string('studentrecords', 'local_enrolstaff'), '', ''));
 
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/unitleaderid', get_string('unitleaderid', 'local_enrolstaff'), '', '19'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/roleids', get_string('roleidssolent', 'local_enrolstaff'), '', '0'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/qaheroleids', get_string('roleidsqahe', 'local_enrolstaff'), '', '0'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/qahecodes', get_string('codesqahe', 'local_enrolstaff'), '', '0'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/excludeshortname', get_string('excludeshortname', 'local_enrolstaff'), '', '0'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/excludefullname', get_string('excludeshortname', 'local_enrolstaff'), '', 'counselling,social work'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/excludeid', get_string('excludeid', 'local_enrolstaff'), '', '328, 22679, 6432'));
-$settings->add(new admin_setting_configtext(
-    'local_enrolstaff/studentrecords', get_string('studentrecords', 'local_enrolstaff'), '', ''));
+    $name = new lang_string('qahecontact', 'local_enrolstaff');
+    $desc = new lang_string('qahecontact_desc', 'local_enrolstaff');
+    $settings->add(new admin_setting_configtext('local_enrolstaff/qahecontact', $name, $desc, ''));
 
-$name = new lang_string('qahecontact', 'local_enrolstaff');
-$desc = new lang_string('qahecontact_desc', 'local_enrolstaff');
-$settings->add(new admin_setting_configtext('local_enrolstaff/qahecontact', $name, $desc, ''));
+    $name = new lang_string('enrolmentnotificationmessageenable', 'local_enrolstaff');
+    $desc = new lang_string('enrolmentnotificationmessageenable_desc', 'local_enrolstaff');
+    $settings->add(new admin_setting_configcheckbox('local_enrolstaff/enrolmentnotificationmessageenable', $name, $desc, 1));
 
-$name = new lang_string('enrolmentnotificationmessageenable', 'local_enrolstaff');
-$desc = new lang_string('enrolmentnotificationmessageenable_desc', 'local_enrolstaff');
-$settings->add(new admin_setting_configcheckbox('local_enrolstaff/enrolmentnotificationmessageenable', $name, $desc, 1));
+    $name = new lang_string('enrolmentnotificationsubject', 'local_enrolstaff');
+    $desc = new lang_string('enrolmentnotificationsubject_desc', 'local_enrolstaff');
+    $default = new lang_string('enrolmentnotificationsubject_default', 'local_enrolstaff');
+    $settings->add(new admin_setting_configtext('local_enrolstaff/enrolmentnotificationsubject', $name, $desc, $default, PARAM_TEXT));
 
-$name = new lang_string('enrolmentnotificationsubject', 'local_enrolstaff');
-$desc = new lang_string('enrolmentnotificationsubject_desc', 'local_enrolstaff');
-$default = new lang_string('enrolmentnotificationsubject_default', 'local_enrolstaff');
-$settings->add(new admin_setting_configtext('local_enrolstaff/enrolmentnotificationsubject', $name, $desc, $default, PARAM_TEXT));
+    $name = new lang_string('enrolmentnotificationmessage', 'local_enrolstaff');
+    $desc = new lang_string('enrolmentnotificationmessage_desc', 'local_enrolstaff');
+    $default = new lang_string('enrolmentnotificationmessage_default', 'local_enrolstaff');
+    $settings->add(new admin_setting_configtextarea(
+        'local_enrolstaff/enrolmentnotificationmessage', $name, $desc, $default, PARAM_TEXT, 120, 15));
 
-$name = new lang_string('enrolmentnotificationmessage', 'local_enrolstaff');
-$desc = new lang_string('enrolmentnotificationmessage_desc', 'local_enrolstaff');
-$default = new lang_string('enrolmentnotificationmessage_default', 'local_enrolstaff');
-$settings->add(new admin_setting_configtextarea(
-    'local_enrolstaff/enrolmentnotificationmessage', $name, $desc, $default, PARAM_TEXT, 120, 15));
-
-$ADMIN->add('localplugins', $settings);
+    $ADMIN->add('localplugins', $settings);
+}
